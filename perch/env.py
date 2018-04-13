25399c9d0bf8959cb87cc31d8469a2ac4e946f86
Joel Green <thejoelgreen@gmail.com>
2018-04-13 15:28:07 -0700

    environment settings and ignored private keys

---
+++ perch/env.py
@@ -0,0 +1,22 @@
+from perch.private import default_aws_access_key_id, default_aws_secret_access_key
+
+default_aws_region = "us-west-2"
+
+
+class Default(object):
+    # AWS Credentials
+    aws_access_key_id = default_aws_access_key_id
+    aws_secret_access_key = default_aws_secret_access_key
+
+    aws_region = default_aws_region
+
+    # Elasticsearch URL
+    elasticsearch_host_url = ""
+
+
+env_mapper = {
+    'default': Default
+}
+
+# What default environment everything should run on
+env = Default
